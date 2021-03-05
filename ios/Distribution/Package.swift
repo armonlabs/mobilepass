20dd8694fad3754edaf928bafa25555c5d7bfdc5
Erinç Çakır <erinc.cakir@armakom.com>
2021-03-05 13:38:55 +0300

    swift package

---
+++ ios/Distribution/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "MobilePass",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "MobilePass", 
+            targets: ["MobilePass"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MobilePassSDK", 
+            path: "MobilePassSDK.xcframework")
+    ]
+)
