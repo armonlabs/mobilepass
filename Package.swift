0edc927e045da8c6abb3547bc3a2492c3ac5df38
Erinç Çakır <erinc.cakir@armakom.com>
2021-03-05 14:59:56 +0300

    changes

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobilePassSDK",
-            url: "https://customers.pspdfkit.com/pspdfkit/xcframework/10.2.1.zip",
+            url: "https://github.com/armonlabs/mobilepass/raw/main/ios/Distribution/MobilePassSDK.xcframework.0.0.1.zip",
             checksum: "f0952cbdf3dfb4af7c2b7a2286a4218a1dca85e275eee8a78a620a0acf8a73aa"),
     ]
 )
