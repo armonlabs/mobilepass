cac79c408e93926cdafb60eeed27a02d80946745
Erinç Çakır <erinc.cakir@armakom.com>
2021-03-10 09:33:17 +0300

    v0.0.3

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobilePassSDK",
-            url: "https://github.com/armonlabs/mobilepass/raw/main/ios/Distribution/MobilePassSDK.xcframework.0.0.2.zip",
-            checksum: "01f8ebadd81158d4758db7043f54f34198aa328b6de9d69e4b53edd4ac66d144"),
+            url: "https://github.com/armonlabs/mobilepass/raw/main/ios/Distribution/MobilePassSDK.xcframework.0.0.3.zip",
+            checksum: "6888b31d44fa84a4e9ce0337e06fcba1341fb9c40a1fb6bd0ff70a19188118dd"),
     ]
 )
