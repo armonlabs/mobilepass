deff5b0df5557d183b998facaefe3209b7c93b7c
Erinç Çakır <erinc.cakir@armakom.com>
2021-06-28 10:40:35 +0300

    [release] new version > 1.1.0

--- android/mobilepasssdk/src/main/java/com/armongate/mobilepasssdk/MobilePass.java
+++ android/mobilepasssdk/src/main/java/com/armongate/mobilepasssdk/MobilePass.java
@@ -30,7 +30,7 @@ public class MobilePass {
     public MobilePass(Context context, Configuration config) {
         mActiveContext = context;
 
-        LogManager.getInstance().info("SDK Version: 1.0.1");
+        LogManager.getInstance().info("SDK Version: 1.1.0");
 
         BaseService.getInstance().setContext(context);
         BluetoothManager.getInstance().setContext(context);
