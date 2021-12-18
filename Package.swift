d0b3150794b5fa6bcc19e41c6169caf1e99af183
Erinç Çakır <erinc.cakir@armongate.com>
2021-12-18 10:33:14 +0300

    [fix] updated swift package url

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobilePassSDK",
-            url: "https://github.com/armonlabs/mobilepass/raw/main/ios/Distribution/MobilePassSDK.xcframework.1.3.1.zip",
+            url: "https://github.com/armonlabs/mobilepass/raw/ios12/ios/Distribution/MobilePassSDK.xcframework.1.3.1.zip",
             checksum: "b92423b2399aaa8bc5845df003043205e4b7c7e0f85f383862201b2f47185528"),
     ]
 )
